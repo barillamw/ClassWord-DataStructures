9ba25069fdedf25e490421a89741326f84d1116f
barillamw <42948039+barillamw@users.noreply.github.com>
2018-09-26 13:39:44 -0400

    Merge branch

--- tests/hw4.cpp
+++ tests/hw4.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include "poly.h"
 
@@ -22,28 +21,3 @@ int main()
 
   return 0;
 }
-=======
-#include <stdio.h>
-#include "poly.h"
-
-int main()
-{
-  List l;
-
-  l.append(StrItem("milk"));
-
-
-
-  iterator it = l.begin();
-  while(!it.end())
-    {
-      Object& copy = it.getItem();
-
-      copy.print();
-
-      it.increment();
-    }
-
-  return 0;
-}
->>>>>>> 6ff87dd704aa03430c0c78af99004cd7c4f28560
