e3205ca605ea7ba078c1402e1e8164b8a71d515b
barillamw <42948039+barillamw@users.noreply.github.com>
2018-09-26 13:33:05 -0400

    Merge branch 'master' of https://github.com/barillamw/ece309-fall18-mbarill

--- tests/hw4.cpp
+++ tests/hw4.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include "poly.h"
 
@@ -21,3 +22,28 @@ int main()
 
   return 0;
 }
+=======
+#include <stdio.h>
+#include "poly.h"
+
+int main()
+{
+  List l;
+
+  l.append(StrItem("milk"));
+
+
+
+  iterator it = l.begin();
+  while(!it.end())
+    {
+      Object& copy = it.getItem();
+
+      copy.print();
+
+      it.increment();
+    }
+
+  return 0;
+}
+>>>>>>> 6ff87dd704aa03430c0c78af99004cd7c4f28560
