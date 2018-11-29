9103fd3cd02613ffd14616071c7f4e58ce365bed
barillamw <42948039+barillamw@users.noreply.github.com>
2018-11-29 17:26:25 -0500

    Update Graph.cpp

--- lib/Graph.cpp
+++ lib/Graph.cpp
@@ -5,15 +5,15 @@
 #include <climits>
 #include <stdio.h>
 
-SparseGraph::SparseGraph(int n) : Graph(n) { nodes = new Node[numNodes]; }
+ece309::SparseGraph::SparseGraph(int n) : Graph(n) { nodes = new Node[numNodes]; }
 
-void SparseGraph::addEdge(int v1, int v2) {
+void ece309::SparseGraph::addEdge(int v1, int v2) {
   nodes[v1].edge.append(v2);
   nodes[v2].edge.append(v1);
 }
 
-bool SparseGraph::isAdjacent(int v1, int v2) const {
-  List::iterator it = nodes[v1].edge.begin();
+bool ece309::SparseGraph::isAdjacent(int v1, int v2) const {
+  ece309::List::iterator it = nodes[v1].edge.begin();
   while (!it.end()) {
     if (it.getItem() == v2) {
       return true;
@@ -23,16 +23,16 @@ bool SparseGraph::isAdjacent(int v1, int v2) const {
   return false;
 }
 
-DenseGraph::DenseGraph(int n) : Graph(n) {
+ece309::DenseGraph::DenseGraph(int n) : Graph(n) {
   edges = new bool[numNodes * numNodes];
   for (int i = 0; i < numNodes * numNodes; i++)
     edges[i] = false;
 }
-void DenseGraph::addEdge(int v1, int v2) {
+void ece309::DenseGraph::addEdge(int v1, int v2) {
   edges[v1 * numNodes + v2] = true;
   edges[v2 * numNodes + v1] = true;
 }
 
-bool DenseGraph::isAdjacent(int v1, int v2) const {
+bool ece309::DenseGraph::isAdjacent(int v1, int v2) const {
   return edges[v1 * numNodes + v2];
 }
