741eedde1fa42e12627c5567277c56ed731ca98a
barillamw <42948039+barillamw@users.noreply.github.com>
2018-11-29 17:41:24 -0500

    Update Queue.cpp

--- lib/Queue.cpp
+++ lib/Queue.cpp
@@ -1,24 +1,24 @@
 #include "Queue.h"
 
-void Queue::push(Item item)
+void ece309::Queue::push(Item item)
 {
   list.append(item);
 }
 
-void Queue::pop()
+void ece309::Queue::pop()
 {
   List::iterator it; // NULL iterator
   // remove from the head of the queue using NULL iterator
   list.removeAfter(it);
 }
 
-Item& Queue::peek()
+Item& ece309::Queue::peek()
 {
   List::iterator it = list.begin();
   return it.getItem();
 }
 
-bool Queue::empty()
+bool ece309::Queue::empty()
 {
   return list.empty();
 }
