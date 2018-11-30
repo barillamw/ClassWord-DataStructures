a5da014e821b3a5a7d36dfaae948bf3f87fd39a0
barillamw <42948039+barillamw@users.noreply.github.com>
2018-11-29 19:17:17 -0500

    Update hw4.cpp

--- tests/hw4.cpp
+++ tests/hw4.cpp
@@ -5,15 +5,15 @@
 int main()
 {
   //declaration of variables and formation of the list
-  pList l;
-  IntItem *one = new IntItem(7);
+  ece309::pList l;
+  ece309::IntItem *one = new IntItem(7);
   l.append(one);
-  Object *two = new StrItem("This is a string");
+  ece309::Object *two = new StrItem("This is a string");
   l.append(two);
-  Object *three = new DblItem(2.0);
+  ece309::Object *three = new DblItem(2.0);
   l.append(three);
 
-  iterator it = l.begin();
+  ece309::iterator it = l.begin();
   while(!it.end())
     {
       Object* copy = it.getItem();
